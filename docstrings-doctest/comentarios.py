789062db94e9410e2ec49713e9401efb656d4114
Nahuel <nahuel.profesor@gmail.com>
2023-08-13 08:13:24 -0300

    primer commit

---
+++ docstrings-doctest/comentarios.py
@@ -0,0 +1,18 @@
+##### Comentarios #####
+
+# Permite hacer comentarios de una sola línea
+
+# Comentarios de múltiples líneas:
+
+"""
+Este comentario posee
+saltos de línea
+'''Podemos utilizar triple comillas simples dentro'''
+"""
+
+'''
+Este comentario también posee
+saltos de línea
+"""Podemos utilizar triple comillas dobles dentro"""
+'''
+
